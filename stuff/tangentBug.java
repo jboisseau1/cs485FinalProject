c66ccf264e85c97220fc953bc90cb14ff14ec0a0
Lorenzo Zamora <lolinzky@Lorenzos-MBP.byod.gmu.edu>
2018-12-10 12:25:03 -0500

    add the Tangent bug file and class under stuff dir

---
+++ stuff/tangentBug.java
@@ -0,0 +1,32 @@
+
+//need to include the other packages still
+package gmu.robot.pioneer;
+package com.brianziman.robotics;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
+public class tangentBug {
+  static long time = 0;
+  public static boolean current_time(long millis)
+  {
+    long current = System.currentTimeMillis();
+    if(time + millis < current)
+    {
+      time = current;
+      return true;
+    }
+    else return false;
+  }
+
+
+  public static void main(String []args) throws Exception
+  {
+      if(args.length < 1)
+      {
+        
+      }
+  }
+}
