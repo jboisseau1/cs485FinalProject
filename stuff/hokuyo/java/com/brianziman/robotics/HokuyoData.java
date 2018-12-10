d9963b3ff6e2095520e8aa62328ce398e8e65b4e
ckidwel <42750860+ckidwel@users.noreply.github.com>
2018-12-10 11:46:39 -0500

    HokuyoData to hold data from robot

---
+++ stuff/hokuyo/java/com/brianziman/robotics/HokuyoData.java
@@ -0,0 +1,43 @@
+
+package com.brianziman.robotics;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public static ArrayList<Integer> data = new ArrayList<Integer>();
+public static ArrayList<Integer> nonmes = new ArrayList<Integer>();
+public static int max, maxindex;
+
+public class Hokuyo{
+
+  public static void checkData(int start, int end, int cluster) {
+
+	for(int i = 0; i < data.size()-1; i++){
+      nonmes.set(i, 0);
+    }
+  
+    data = doDistanceCommand(start, end, cluster);
+    getLongestDist();
+
+  }
+
+  public static void getLongestDist(){
+    max = -1;
+    maxindex = -1;
+
+    for(int i = 0; i<data.size()-1;i++){
+      if(data.get(i) < 20){
+
+        nonmes.index(i) = 1;
+      }
+      else(data.get(i) > max){
+        max = data.get(i);
+        maxindex = i;
+      }
+    }
+  }
+
+
+
+}
